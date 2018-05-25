c64a2fda3cf08b8cdd946ec29928d13f3962df92
Amol <amolborse699@gmail.com>
2018-05-26 00:22:28 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'app';
+
+  itemList = [];
+  selectedItems = [];
+  settings = {};
+
+  constructor() { }
+
+  ngOnInit() {
+
+    this.itemList = [
+      { 'id': 1, 'itemName': 'India' },
+      { 'id': 2, 'itemName': 'Singapore' },
+      { 'id': 3, 'itemName': 'Australia' },
+      { 'id': 4, 'itemName': 'Canada' },
+      { 'id': 5, 'itemName': 'South Korea' },
+      { 'id': 6, 'itemName': 'Brazil' }
+    ];
+
+    this.selectedItems = [
+      { 'id': 1, 'itemName': 'India' },
+      { 'id': 2, 'itemName': 'Singapore' },
+      { 'id': 3, 'itemName': 'Australia' },
+      { 'id': 4, 'itemName': 'Canada' }];
+    this.settings = {
+      text: 'Select Countries',
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      classes: 'myclass custom-class'
+    };
+  }
+  onItemSelect(item: any) {
+    console.log(item);
+    console.log(this.selectedItems);
+  }
+  OnItemDeSelect(item: any) {
+    console.log(item);
+    console.log(this.selectedItems);
+  }
+  onSelectAll(items: any) {
+    console.log(items);
+  }
+  onDeSelectAll(items: any) {
+    console.log(items);
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AngularMultiSelectModule } from 'angular2-multiselect-dropdown';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+     ReactiveFormsModule,
+    AngularMultiSelectModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [
+    AngularMultiSelectModule
+  ]
 })
 export class AppModule { }
